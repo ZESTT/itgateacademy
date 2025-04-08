4894fa6409ecd2052ab921b4cc64cae22d237897
YN <yn@192.168.1.7>
2025-04-08 11:52:34 +0200

    Initial commit of Next.js project

---
+++ components/main/Navbar.tsx
@@ -0,0 +1,51 @@
+import { Socials } from "@/constants";
+import Image from "next/image";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
+      <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
+        <a
+          href="#about-me"
+          className="h-auto w-auto flex flex-row items-center"
+        >
+          <Image
+            src="/log.png"
+            alt="logo"
+            width={90}
+            height={90}
+            className="cursor-pointer hover:animate-slowspin"
+          />
+
+          <span className="font-bold ml-[10px] hidden md:block text-gray-200">
+          It Gate Academy
+          </span>
+        </a>
+
+        <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
+          <div className="flex items-center justify-center w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
+            <a href="https://www.itgateacademy.com" className="cursor-pointer">
+              About Us
+            </a>
+         
+          </div>
+        </div>
+
+        <div className="flex flex-row gap-5">
+          {Socials.map((social) => (
+            <Image
+              src={social.src}
+              alt={social.name}
+              key={social.name}
+              width={24}
+              height={24}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ components/main/Projects.tsx
@@ -0,0 +1,115 @@
+import React from "react";
+import ProjectCard from "../sub/ProjectCard";
+
+const Projects = () => {
+  return (
+    <div
+      className="flex flex-col items-center justify-center py-20"
+      id="projects"
+    >
+      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+        You will Do this Real Projects
+      </h1>
+      <div className="h-full w-full flex flex-col lg:flex-row md:flex-row gap-10 px-10">
+        <ProjectCard
+          src="/NextWebsite.png"
+          title="Modern Next.js Portfolio"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+        <ProjectCard
+          src="/bakery.jpg"
+          title="Interactive Bakery Website "
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+        <ProjectCard
+          src="/m.jpg"
+          title="Movies Site"
+          description="video subscription service. The homepage features a prominent title 'REBUNEKA THE DOLL' with a video thumbnail"
+        />
+         <ProjectCard
+          src="/e.jpg"
+          title="E-Commerce Site"
+          description="This e-commerce web page is designed to showcase products, special selections, blog
+ posts, and other shopping-related features. The main sections include:
+ "
+        />
+      </div>
+
+
+      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
+        <ProjectCard
+          src="/dash.jpg"
+          title="Dashboard System"
+          description="dashboard interface designed for website administration purposes. The dashboard includes a navigation sidebar with options ,"
+        />
+        <ProjectCard
+          src="/profile.jpg"
+          title="Interactive Website "
+          description="developer's profile page showcasing their skills, projects, and social media links. To create a similar profile page, you can follow these steps:"
+        />
+        <ProjectCard
+          src="/4.jpg"
+          title="Portfolio Website"
+          description="developer's profile page showcasing their skills, projects, and social media links. To create a similar profile page, you can follow these steps:"
+        />
+         <ProjectCard
+          src="/p.jpg"
+          title="Portfolio Site"
+          description="developer's profile page showcasing their skills, projects, and social media links. To create a similar profile page, you can follow these steps:"
+        />
+
+
+      </div>
+
+
+
+      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
+      
+        <ProjectCard
+          src="/hh.jpg"
+          title="Gemini Clone"
+          description=""
+        
+        />
+        <ProjectCard
+          src="/pro.jpg"
+          title="Product Mangement System"
+          description=""
+        />
+        
+
+
+      </div>
+
+
+
+      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+          Our Students Ready to Work
+      </h1>
+      <div className="h-full w-full flex flex-col lg:flex-row md:flex-row gap-10 px-10">
+        <ProjectCard
+          src="/s1.jpg"
+          title=""
+          description=""
+        />
+        <ProjectCard
+          src="/s2.jpg"
+          title=""
+          description=""
+        />
+        <ProjectCard
+          src="/s3.jpg"
+          title=""
+          description=""
+        />
+         <ProjectCard
+          src="/s4.jpg"
+          title=""
+          description=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
